b997805f638f7c8bb2777a9c246d8be3e942a33b
Jared Dantis <jareddantis@gmail.com>
2019-07-12 15:38:14 +0800

    compile.sh: Restore Linux & tmpfs support

--- compile.sh
+++ compile.sh
@@ -32,6 +32,7 @@ function help_menu() {
     echo "  -a  | --arch:        Possible values: arm, arm-android, or arm64. This is the toolchain's target architecture."
     echo
     echo "${BOLD}OPTIONAL PARAMETERS:${RST}"
+    echo "  -nt | --no-tmpfs:    Do not use tmpfs for building (useful if you don't have much RAM)."
     echo "  -nu | --no-update:   Do not update the downloaded components before building (useful if you have slow internet)."
     echo "  -p  | --package:     Possible values: gz or xz. Compresses toolchain after build."
     echo "  -V  | --verbose:     Make script print all output, not just errors and the ending information"
@@ -100,15 +101,40 @@ function format_time() {
     echo "${TIME_STRING}"
 }
 
+# Check if user needs to enter sudo password or not
+function check_sudo() {
+    echo
+    echo "Checking if sudo is available, please enter your password if a prompt appears!"
+    if ! sudo -v 2>/dev/null; then
+        warn "Sudo is not available! Disabling the option for tmpfs..." -n
+        NO_TMPFS=true
+    fi
+}
+
 # Initial setup
 function setup_variables() {
     # Start of script
     START=$(date +%s)
     BUILD_DATE=$(TZ=UTC date +%Y%m%d)
 
-    # Check if running on macOS
+    # Set multithread and config flags per platform
     PLATFORM=$(uname)
-    [[ ${PLATFORM} != "Darwin" ]] && die "This script is for macOS only."
+    if [[ ${PLATFORM} = "Darwin" ]]; then
+        CORES=$(sysctl -a | grep machdep.cpu | grep core_count | awk '{print $2}')
+        THREADS=$(sysctl -a | grep machdep.cpu | grep thread_count | awk '{print $2}')
+        MK="gmake -j$(((CORES * THREADS) / 2))"
+        HOST_PLATFORM="x86_64-apple-darwin"
+        CONFIGURATION=( "--with-host-libstdcxx='-static-libstdc++ -Wl,-lstdc++ -lm'" )
+
+        # Disable tmpfs
+        warn "tmpfs not available on Mac, disabling..."
+        NO_TMPFS=true
+    else
+        THREADS="$(nproc --all)"
+        MK="make -j${THREADS}"
+        HOST_PLATFORM="x86_64-linux-gnu"
+        CONFIGURATION=( "--with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm'" )
+    fi
 
     # Build root
     ROOT=${PWD}
@@ -129,9 +155,9 @@ function setup_variables() {
     MPFR="4.0.2"
 
     # Configuration variables
-    CONFIGURATION=(
-        "--host=x86_64-apple-darwin"
-        "--build=x86_64-apple-darwin"
+    CONFIGURATION+=(
+        "--host=${HOST_PLATFORM}"
+        "--build=${HOST_PLATFORM}"
         "--with-pkgversion='${GCC}-${BUILD_DATE}'"
         "--with-gcc-version=${GCC}"
         "--with-binutils-version=${BINUTILS}"
@@ -148,11 +174,7 @@ function setup_variables() {
         "--enable-threads"
         "--enable-ld=default"
         "--enable-graphite=yes"
-        "--with-host-libstdcxx='-static-libstdc++ -Wl,-lstdc++ -lm'"
     )
-    CORES=$(sysctl -a | grep machdep.cpu | grep core_count | awk '{print $2}')
-    THREADS=$(sysctl -a | grep machdep.cpu | grep thread_count | awk '{print $2}')
-    JOBS="-j$(((CORES * THREADS) / 2))"
 }
 
 # Parse parameters
@@ -163,6 +185,7 @@ function parse_parameters() {
             "-a"|"--arch") shift && ARCH=${1} ;;
 
             # OPTIONAL FLAGS
+            "-nt"|"--no-tmpfs") NO_TMPFS=true ;;
             "-nu"|"--no-update") NO_UPDATE=true ;;
             "-p"|"--package") shift && COMPRESSION=${1} ;;
             "-V"|"--verbose") VERBOSE=true ;;
@@ -174,6 +197,7 @@ function parse_parameters() {
         shift
     done
 
+    [[ -z ${NO_TMPFS} ]] && [[ ${PLATFORM} != "Darwin" ]] && check_sudo
     [[ -z ${VERBOSE} ]] && exec 6>&2 5>&1 &>/dev/null
 
     # Default values
@@ -194,9 +218,17 @@ function parse_parameters() {
     esac
 }
 
+# Unmount tmpfs
+function unmount_tmpfs() {
+    if [[ -z ${NO_TMPFS} ]]; then
+        sudo umount -f "${ROOT}/out/build" 2>/dev/null
+    fi
+}
+
 # Clean up from a previous compilation
 function clean_up() {
     header "CLEANING UP"
+    unmount_tmpfs
     rm -rf "${ROOT}/out"
     [[ -d "${ROOT}/out" ]] && die "Failed to remove 'out'. Please check if you have proper permissions."
     echo "Clean up successful!"
@@ -204,13 +236,16 @@ function clean_up() {
 
 # Setup source folders and build folders
 function setup_env() {
-    # Require GNU make, sed, bison, m4
-    [[ -z "$(command -v gmake)" ]] && die "GNU Make not found in your $PATH. Please install it first."
     [[ -z "$(command -v pigz)" ]] && die "pigz not found in your $PATH. Please install it first."
-    [[ ! -d /usr/local/opt/gnu-sed ]] && die "Please install gnu-sed with Homebrew first."
-    [[ ! -d /usr/local/opt/bison ]] && die "Please install bison with Homebrew first."
-    [[ ! -d /usr/local/opt/m4 ]] && die "Please install m4 with Homebrew first."
-    export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/bison/bin:/usr/local/opt/m4/bin:${INSTALL}/bin:${PATH}"
+
+    if [[ ${PLATFORM} = "Darwin" ]]; then
+        # Require GNU make, sed, bison, m4
+        [[ -z "$(command -v ${MK})" ]] && die "GNU Make not found in your $PATH. Please install it first."
+        [[ ! -d /usr/local/opt/gnu-sed ]] && die "Please install gnu-sed with Homebrew first."
+        [[ ! -d /usr/local/opt/bison ]] && die "Please install bison with Homebrew first."
+        [[ ! -d /usr/local/opt/m4 ]] && die "Please install m4 with Homebrew first."
+        export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/bison/bin:/usr/local/opt/m4/bin:${INSTALL}/bin:${PATH}"
+    fi
 
     # Prepare sysroot
     INSTALL=${ROOT}/out/${TARGET}-${GCC}
@@ -223,6 +258,11 @@ function setup_env() {
         "--with-sysroot=${SYSROOT}"
         "--with-gxx-include-dir=${SYSROOT}/c++"
     )
+
+    # Prepare tmpfs
+    if [[ -z ${NO_TMPFS} ]]; then
+        sudo mount -t tmpfs -o rw none "${ROOT}/out/build"
+    fi
 }
 
 # Download tarballs and update isl/osl
@@ -297,8 +337,8 @@ function build_graphite() {
     mkdir -p "${ROOT}/out/build/cloog-current"
     cd "${ROOT}/out/build/cloog-current" || die "Build folder does not exist!"
     "${CLOOG_DIR}/configure" --prefix="${CLOOG_PREFIX}" --with-isl=bundled --with-osl=bundled --with-gmp=system
-    gmake ${JOBS} || die "Error while building CLooG!" -n
-    gmake install ${JOBS} || die "Error while building CLooG!" -n
+    ${MK} || die "Error while building CLooG!" -n
+    ${MK} install || die "Error while building CLooG!" -n
 }
 
 # Build toolchain
@@ -314,8 +354,8 @@ function build_tc() {
         *) "${ROOT}/build/configure" "${CONFIGURATION[@]}" ;;
     esac
 
-    gmake ${JOBS} || die "Error while building toolchain!" -n
-    gmake install ${JOBS} || die "Error while building toolchain!" -n
+    ${MK} || die "Error while building toolchain!" -n
+    ${MK} install || die "Error while building toolchain!" -n
 }
 
 # Package toolchain
@@ -366,7 +406,8 @@ function ending_info() {
     echo "\a"
 }
 
-trap 'die "Manually aborted!" -n' SIGINT SIGTERM
+trap 'unmount_tmpfs; die "Manually aborted!" -n' SIGINT SIGTERM
+trap 'unmount_tmpfs' EXIT
 setup_variables
 parse_parameters "${@}"
 clean_up
